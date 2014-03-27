c71ff8af90b693a174b03dc719e91653480f4343
Can <can@celwk.com>
2014-03-28 02:16:19 +0800

    Initial Commit

---
+++ CALayer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CALayer
+//
+//  Created by Can EriK Lu on 3/28/14.
+//  Copyright (c) 2014 Can EriK Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CALayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CALayer
+//
+//  Created by Can EriK Lu on 3/28/14.
+//  Copyright (c) 2014 Can EriK Lu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
